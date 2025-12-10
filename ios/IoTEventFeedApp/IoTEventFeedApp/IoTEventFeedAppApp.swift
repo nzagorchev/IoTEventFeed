04142b83f006a3ebcb00d0c6db734d293c47939c
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-12-10 17:34:28 +0200

    feat: create iOS project

---
+++ ios/IoTEventFeedApp/IoTEventFeedApp/IoTEventFeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoTEventFeedAppApp.swift
+//  IoTEventFeedApp
+//
+//  Created by Nikola Zagorchev on 10.12.25.
+//
+
+import SwiftUI
+
+@main
+struct IoTEventFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
